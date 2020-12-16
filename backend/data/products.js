7093969b0ea721e404123cf0a74c300e04b905b5
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-12-16 17:20:14 +0300

    project files added

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+  
+    name: 'Canon 850D',
+    image: '/images/camera1.png',
+    description:
+      'The Canon EOS 850D (called Canon T8i in some regions) is a DSLR (Digital Single Lens Reflex) camera that was officially introduced in February 2020 and is equipped with an APS-C sensor. It offers a resolution of 24 megapixel',
+    brand: 'Canon',
+    category: 'Electronics',
+    price: 700.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'Canon 90D',
+    image: '/images/camera2.png',
+    description:
+      'The Canon EOS 90D camera features a powerful 32.5 Megapixel CMOS (APS-C) sensor that can capture high-resolution images. Utilizing a sophisticated light-capturing design, it provides high sensitivity for high ISO capture with reduced noise, delivering clear, detailed results even in low light.',
+    brand: 'Canon',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera3.png',
+    description:
+      'The Canon EOS 80D is an enthusiast-level DSLR, and the successor to the 70D. It sports a new 24MP APS-C CMOS sensor which, like the 70D, offers Canons Dual Pixel on-sensor phase-detection autofocus system',
+    brand: 'Canon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'Nikon',
+    image: '/images/camera4.png',
+    description:
+      'is an enthusiast-level DSLR, and the successor to the 70D. It sports a new 24MP APS-C CMOS sensor which, like the 70D, offers Canons Dual Pixel on-sensor phase-detection autofocus system',
+    brand: 'Nikon',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'Canon',
+    image: '/images/camera5.png',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Canon',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    
+    name: 'Nikon',
+    image: '/images/camera6.png',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Nikon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
